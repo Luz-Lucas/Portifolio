814bdacedd29ddfe4b069628bd8036f01dbff6f3
Luz-Lucas <lucaspmluz2145@gmail.com>
2025-11-11 14:59:57 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default {
-  base: 'Portfolio',
-}
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
